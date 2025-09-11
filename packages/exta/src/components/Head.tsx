9f0407d5eac99873417ff8d01c4abe86956073c3
r.ehdals <kangdongmandoo@gmail.com>
2025-09-11 22:33:33 +0900

    fix: fixed head duplication

--- packages/exta/src/components/Head.tsx
+++ packages/exta/src/components/Head.tsx
@@ -1,19 +1,39 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement, useEffect, cloneElement } from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 
 export function Head({ children }: { children: ReactElement | ReactElement[] }) {
-  if (typeof window === 'undefined') {
-    global.__EXTA_SSR_DATA__.head.push(renderToStaticMarkup(children));
+  const isServer = typeof window === 'undefined';
 
+  if (isServer) {
+    if (global.__EXTA_SSR_DATA__) {
+      const wrapped = Array.isArray(children)
+        ? children.map((c, i) =>
+            cloneElement(c, { ...({ 'data-ssr-head': '', key: i } as any) }),
+          )
+        : cloneElement(children as React.ReactElement<any>, {
+            ...({ 'data-ssr-head': '' } as any),
+          });
+
+      global.__EXTA_SSR_DATA__.head.push({
+        type: 'html',
+        data: renderToStaticMarkup(wrapped).replace(/data-ssr-head=""/g, 'data-ssr-head'),
+      });
+    }
     return null;
   }
 
   useEffect(() => {
+    document.head
+      .querySelectorAll('[data-ssr-head]')
+      .forEach((el) => el.parentNode?.removeChild(el));
+
+    console.log();
+
     const html = renderToStaticMarkup(children);
     const template = document.createElement('template');
     template.innerHTML = html;
-
     const nodes = Array.from(template.content.childNodes);
+
     nodes.forEach((node) => document.head.appendChild(node));
 
     return () => {
